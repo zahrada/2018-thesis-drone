8d737550094d949ada36ea0fb7783772902555e8
zahrada <32935470+zahrada@users.noreply.github.com>
2019-03-08 17:07:33 +0100

    app

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/ControlDrone.java
@@ -0,0 +1,218 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+
+public class ControlDrone extends AppCompatActivity implements OnMapReadyCallback {
+
+    //Google maps
+    private MapView mMapView;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private static final String TAG = "ControlDrone";
+
+
+    BluetoothDevice mBTDevice;
+    private BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+    public ArrayList<BluetoothDevice> mBTDevices = new ArrayList<>();
+    BluetoothConnectionService mBluetoothConnection;
+
+    TextView btName;
+
+    private SeekBar seekBar;
+    //private ProgressBar progressBar;
+    private TextView textViewSeekBar;
+
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    //joystick
+    int joyXm, joyY;
+
+    int speed = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.control_drone);
+
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
+
+        //joystick
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textViewSeekBar = (TextView) findViewById(R.id.seekBarTextView);
+
+        // Initialize the textview with '0'.
+        textViewSeekBar.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
+
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textViewSeekBar.setText("Covered: " + progress + "/" + seekBar.getMax());
+            }
+        });
+        //bluetooth
+        btName = (TextView) findViewById(R.id.btname);
+
+        mBTDevices.clear();
+        if (bAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Bluetooth Not Supported", Toast.LENGTH_SHORT).show();
+        } else {
+            Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+                    mBTDevices.add(device);
+                }
+            }
+        }
+
+        int pos = getIntent().getExtras().getInt("pos");
+
+        mBTDevice = mBTDevices.get(pos);
+
+        btName.setText(mBTDevice.getName());
+
+        Log.d(TAG, "import text" + mBTDevice.getName());
+        Toast.makeText(getApplicationContext(), mBTDevice.getName(), Toast.LENGTH_SHORT).show();
+/*
+        mBTDevice.createBond();
+
+        mBluetoothConnection = new BluetoothConnectionService(ControlDrone.this);
+
+        mBluetoothConnection.startClient(mBTDevice,MY_UUID_INSECURE);
+*/
+        JoystickView joystick = (JoystickView) findViewById(R.id.joystickView);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                // do whatever you want
+                btName.setText(angle + " " + strength);
+            }
+        });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+
+        Location location;
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else if (map != null) {
+            map.setMyLocationEnabled(true);
+        }
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(50,14), 1000));
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+}
